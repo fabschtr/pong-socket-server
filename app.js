03b9bdc28ad443a8508f560d26e0051f3b3446f5
Fabian Schneider <fabian@seerow.ch>
2018-12-24 16:14:26 +0100

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 3000;
 const index = require("./routes/index");
 
 const app = express();
